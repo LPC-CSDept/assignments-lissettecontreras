8e476735b873fb0d04280d8cd0bfd652916e5817
lissettecontreras <lissettecontreras245@gmail.com>
2022-02-22 21:56:15 -0800

    quiz3

--- Quiz3.cpp
+++ Quiz3.cpp
@@ -3,12 +3,14 @@ using namespace std;
 
 int main()
 {
-    double x = rand(), y = rand(), z = rand();
+    double x = rand();
+    double y = rand();
+    double z = rand();
     double sum = x + y + z;
     double average = sum / 3;
-    cout << setprecision(5) << x << endl;
-    cout << setprecision(5) << y << endl;
-    cout << setprecision(5) << z << endl;
-    cout << setprecision(5) << sum << endl;
-    cout << setprecision(5) << average << endl;
+    cout <<setprecision(5) << x << endl;
+    cout <<setprecision(5) << y << endl;
+    cout <<setprecision(5) << z << endl;
+    cout <<setprecision(5) << sum << endl;
+    cout <<setprecision(5) << average << endl;
 }
