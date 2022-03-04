88cbf0c2e8c28d079dc130c5c10c03f8e5df8ebc
lissettecontreras <lissettecontreras245@gmail.com>
2022-03-03 23:43:20 -0800

    q4done

--- Q42.cpp
+++ Q42.cpp
@@ -3,5 +3,31 @@ using namespace std;
 
 int main()
 {
+    int weight;
+    int distance;
+    cout << "Enter weight of package: ";
+    cin >> weight;
+    if (weight <= 0 || weight > 20){
+        cout << "\nIllegal weight" << endl;
+        return 0;
+    }
+    cout << "Enter distance to be traveled: ";
+    cin >> distance;
+    if (distance < 10 || distance > 3000){
+        cout << "\nIllegal distance" << endl;
+        return 0;
+    }
+    if (weight <= 2){
+        cout << "\nCharge: $" << (distance/500) * 1.10 << endl;
+    }
+    if (weight > 2 && weight <= 6){
+        cout << "\nCharge: $" << (distance/500) * 2.20 << endl;
+    }
+    if (weight > 6 && weight <= 10){
+        cout << "\nCharge: $" << (distance/500) * 3.70 << endl;
+    }
+    if (weight > 10 && weight <= 20){
+        cout << "\nCharge: $" << (distance/500) * 4.80 << endl;
+    }
     
 }
