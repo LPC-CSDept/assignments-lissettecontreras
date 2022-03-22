5bb918670a125d6d07883e188fbfc263968db386
lissettecontreras <lissettecontreras245@gmail.com>
2022-03-22 13:40:00 -0700

    done

--- quiz5-2q3.cpp
+++ quiz5-2q3.cpp
@@ -6,6 +6,18 @@ int main()
     int begin;
     int end;
     cout << "Enter the range begin and end: ";
-    cin >> begin 
+    cin >> begin >> endl;
+   for (int i = begin; i << end; i++){
+       bool is_prime = true;
+       for (int c = 2; c <= i/2; c++){
+           if i % c == 0){
+               is_prime = false;
+           }
+       }
+
+   }
+
+   )
+    return 0;
     
 }
