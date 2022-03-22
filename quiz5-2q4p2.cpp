c209f877e0e7331fc4d84046af7c874b5b69826a
lissettecontreras <lissettecontreras245@gmail.com>
2022-03-22 14:17:52 -0700

    done

--- quiz5-2q4p2.cpp
+++ quiz5-2q4p2.cpp
@@ -1,2 +1,13 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
+
+int main () {
+    ofstream myfile ("employee.txt");
+    string line;
+    cout << "ID\t\tName\t\tDepartment\t\tSalary" << endl;
+    while (getline(myfile, line)){
+        cout << line;
+    }
+    return 0
+}
