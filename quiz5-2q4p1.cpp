51af3759d25234432b08840b8641863df67e0682
lissettecontreras <lissettecontreras245@gmail.com>
2022-03-22 14:17:37 -0700

    done

--- quiz5-2q4p1.cpp
+++ quiz5-2q4p1.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 
-int main()
-{
-    
-    return 0;
+int main () {
+  ofstream myfile;
+  int id;
+  string name;
+  string department;
+  double salary;
+  myfile.open ("employee.txt");
+  int employees;
+  cin >> employees;
+  myfile << employees << endl;
+  for (int i = 0; i < employees; i++){
+      cin >> id >> name >> department >> salary;
+      myfile << id << " " << name << " " << department << " " << salary << endl;
+  }
+  myfile.close();
+  return 0;
 }
