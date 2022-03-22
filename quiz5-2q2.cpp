9d7d1fc528479c5bcce81a8712260d9c1a9497d1
lissettecontreras <lissettecontreras245@gmail.com>
2022-03-22 13:13:30 -0700

    done

--- quiz5-2q2.cpp
+++ quiz5-2q2.cpp
@@ -5,8 +5,19 @@ int main()
 {
     int N;
     int M;
-    cout << "Enter your base number";
+    cout << "Enter your base number: ";
     cin >> N;
+    cout << "\nEnter your exponent: ";
+    cin >> M;
+
+    cout << N << " to the power number: 1";
+    for (int i = 1; i <= M, i++)(
+        if (i == 0){
+            cout << "1"
+        }
+        else if (i ==1)
+        cout << 
+    )
 
 
     return 0;
