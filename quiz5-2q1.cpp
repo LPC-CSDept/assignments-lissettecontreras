9845ae7504f6f13ece2b31fdc864f55bff16e394
lissettecontreras <lissettecontreras245@gmail.com>
2022-03-22 12:55:30 -0700

    done

--- quiz5-2q1.cpp
+++ quiz5-2q1.cpp
@@ -3,10 +3,14 @@
 #include <ctime>
 
 using namespace std;
-int main()
-{ 
-    int randnumbers[5];
-    srand(time(0)); 
-    int rdnum
 
+int main()
+{
+    int randnumbers[5];
+    srand(time(0));
+    int rdnum;
+    for (int i = 0; i <5; i++){
+        rdnum = rand()%100;
+        randnumbers[i] = rdnum;
+    
 }
