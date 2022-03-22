4603582a144827ee6dc4e63b08901be684952620
lissettecontreras <lissettecontreras245@gmail.com>
2022-03-22 13:07:17 -0700

    done

--- quiz5-2q1.cpp
+++ quiz5-2q1.cpp
@@ -12,5 +12,17 @@ int main()
     for (int i = 0; i <5; i++){
         rdnum = rand()%100;
         randnumbers[i] = rdnum;
-    
+    }
+    cout << "The Generated Random Numbers: " << randnumbers[0];
+    for (int i = 1; i <5; i++){
+        cout << ", " << randnumbers[i];
+    }
+    int temp = 100;
+    for (int c = 0; c < sizeof(randnumbers)/sizeof(randnumbers[0]); c++){
+        if (randnumbers[c] < temp){
+            temp = randnumbers[c];
+        }
+    }
+    cout << "\nThe Minimum Value: " << temp << endl;
+    return 0;
 }
