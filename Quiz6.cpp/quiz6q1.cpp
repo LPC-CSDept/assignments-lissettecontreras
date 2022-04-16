b31ac64d341b6f776cae6b8c90010d18472fe32c
lissettecontreras <lissettecontreras245@gmail.com>
2022-04-15 20:46:03 -0700

    done

--- Quiz6.cpp/quiz6q1.cpp
+++ Quiz6.cpp/quiz6q1.cpp
@@ -2,6 +2,13 @@
 using namespace std;
 void swapTwoValues(int n1, int n2)
 {
-
+    int temp = n1;
+    n1 = n2;
+    n2 = temp;
 }
 void swapTwoValues(float f1, float f2)
+{
+    float temp = f1;
+    f1 = f2;
+    f2 = temp;
+}
