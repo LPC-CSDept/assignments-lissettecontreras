45130a3ce2fb7a59d64f2b42cc1e46731335a741
lissettecontreras <lissettecontreras245@gmail.com>
2022-04-19 16:14:17 -0700

    done

--- midtermexam.cpp/q4.cpp
+++ midtermexam.cpp/q4.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
+#include <ctime>
 
 using namespace std;
 
-int getRdnum()
-{
-    
+int getRdnum(){
+    return rand()%100 + 1;
+}
+
+int getRdnum(int n2){
+    return rand()%(n2) + 1;
+}
+
+int getRdnum(int n1, int n2){
+    return rand()%(n2 - n1 + 1) + n1;
 }
