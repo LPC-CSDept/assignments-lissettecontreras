39848e2b91ee5058c3b11609a92ba16bb72bb27e
lissettecontreras <lissettecontreras245@gmail.com>
2022-04-19 17:45:19 -0700

    done

--- midtermexam.cpp/q2.cpp
+++ midtermexam.cpp/q2.cpp
@@ -20,4 +20,10 @@ int main(){
             int score2 = stoi(helper.at(2));
             int sum = score1 + score2;
             double avg = double(sum)/2;
+    helper.push_back(to_string(avg));
+            cout << "Student name: " << helper.at(0) << "\tScore 1: "
+            << score1 << "\tScore 2: " << score2 << "\tSum: " << sum
+            << "\tAvg: " << avg << endl;
+            student_list.push_back(helper);
+            helper.clear();
 
