c93785faa256e02b0c3248f07c6dcae908d8b567
lissettecontreras <lissettecontreras245@gmail.com>
2022-04-19 17:57:16 -0700

    done

--- midtermexam.cpp/q1.cpp
+++ midtermexam.cpp/q1.cpp
@@ -3,14 +3,13 @@
 using namespace std;
 int main()
 {
-    const int N=5, M=5
-    int i, j;
-    for (i-0; i<N; i++)(
-        for (j-i;j < M-i; j++){
-            cout << "(" << i<< ", " << j << ") ";
-        }
-    cout << endl;
-}
+    const  int N=5, M=5;
+    int     i, j;
+    for(i=0;i<N;i++) {
+        for(j=i; j<M-i; j++)
+            cout << " (" << i << "," << j << ") " ;
+        cout << endl;
+    }
+    return 0;
 
-return 0;
 }
