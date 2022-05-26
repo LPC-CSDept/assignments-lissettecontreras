621eb56ce6730138cba96bfcbc4e39a13e36b71b
lissettecontreras <lissettecontreras245@gmail.com>
2022-05-26 09:47:09 -0700

    done

--- final exam/q3.cpp
+++ final exam/q3.cpp
@@ -1,3 +1,10 @@
+#include <iomanip>
 #include <iostream>
 using namespace std;
+
+int main() {
+    const int SIZE1=5;
+    const int SIZE2=10;
+    int numbers1[SIZE1] = {2, 3, 5, 9, 10};
+    int numbers2[SIZE2] = {1, 2, 3, 5, 9, 10, 11, 13, 15, 17};
 
